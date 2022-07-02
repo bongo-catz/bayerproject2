04623fa225e615ec4591238dd2f760c5eea8d9c7
bongo-catz <69369756+bongo-catz@users.noreply.github.com>
2022-07-01 21:41:36 -0700

    Add files via upload

---
+++ src/routes.js
@@ -0,0 +1,63 @@
+// Material Dashboard 2 React layouts
+import Home from "layouts/home";
+import Tables from "layouts/tables";
+import Query from "layouts/query";
+import Add from "layouts/add";
+import SignIn from "layouts/authentication/sign-in";
+import SignUp from "layouts/authentication/sign-up";
+
+// @mui icons
+import Icon from "@mui/material/Icon";
+
+const routes = [
+  {
+    type: "collapse",
+    name: "Home",
+    key: "home",
+    icon: <Icon fontSize="small">home</Icon>,
+    route: "/home",
+    component: <Home />,
+  },
+  {
+    type: "collapse",
+    name: "Tables",
+    key: "tables",
+    icon: <Icon fontSize="small">table_view</Icon>,
+    route: "/tables",
+    component: <Tables />,
+  },
+  {
+    type: "collapse",
+    name: "Query",
+    key: "query",
+    icon: <Icon fontSize="small">search</Icon>,
+    route: "/query",
+    component: <Query />,
+  },
+  {
+    type: "collapse",
+    name: "Add",
+    key: "adde",
+    icon: <Icon fontSize="small">add</Icon>,
+    route: "/add",
+    component: <Add />,
+  },
+  {
+    type: "collapse",
+    name: "Sign In",
+    key: "sign-in",
+    icon: <Icon fontSize="small">login</Icon>,
+    route: "/authentication/sign-in",
+    component: <SignIn />,
+  },
+  {
+    type: "collapse",
+    name: "Sign Up",
+    key: "sign-up",
+    icon: <Icon fontSize="small">assignment</Icon>,
+    route: "/authentication/sign-up",
+    component: <SignUp />,
+  },
+];
+
+export default routes;
